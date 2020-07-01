b20e18c9a89b852c855b156412d234c269611f33
TDM <63006990+Tharindu-dart@users.noreply.github.com>
2020-07-01 17:53:38 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,100 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          FirebaseUser user = snapshot.data;
+          if (user == null) {
+            return SignInPage();
+          }
+          return HomePage();
+        } else {
+          return Scaffold(
+            body: Center(
+              child: Text("Main wait"),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class SignInPage extends StatelessWidget {
+  Future<void> _signInAnonymously() async {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } catch (e) {
+      print(e); // TODO: show dialog with error
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _signInAnonymously(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState != ConnectionState.active) {
+          return Container(
+            child: Center(
+              child: Text("Loading..."),      // !!! This will need internet check
+            ),
+          );
+        } else {
+          return HomePage();
+        }
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print(e); // TODO: show dialog with error
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Logout',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: _signOut,
+          ),
+        ],
+      ),
+    );
+  }
+}
